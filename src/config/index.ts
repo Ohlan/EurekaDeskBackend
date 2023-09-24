6088243e69a9279b475a1490c1c3812cec32758c
Pushpendra Kumar <pushpendra.kumar@1mg.com>
2023-09-25 01:39:50 +0530

    backend for eurekadesk added

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const MONGO_URI = 'mongodb+srv://admin:abcd1234@cluster0.tlxzrel.mongodb.net/?retryWrites=true&w=majority';
+export const APP_SECRET = '238745623hsdf'
+
+export const PORT = process.env.PORT || 8000;
