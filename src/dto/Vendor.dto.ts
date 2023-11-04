4c9b1ae25fc030f8bf413c355ab5c8a150067e21
Pushpendra Kumar <pushpendra.kumar@1mg.com>
2023-11-04 14:27:40 +0530

    fixed typo erros

---
+++ src/dto/Vendor.dto.ts
@@ -0,0 +1,68 @@
+import { IsEmail, Length } from "class-validator";
+
+export class CreateVandorInput{
+
+    @Length(4, 15)
+    name: string;
+
+    @Length(3, 15)
+    ownerName: string;
+
+    foodType: [string];
+
+    pincode: string;
+
+    address: string;
+
+    @Length(10, 10)
+    phone: string;
+
+    @IsEmail()
+    email: string;
+
+    @Length(6, 15)
+    password: string;
+}
+
+export class EditVendorInput{
+    @Length(4, 15)
+    name: string;
+
+    address: string;
+
+    @Length(10, 10)
+    phone: string;
+
+    foodType:[string]
+}
+
+export class VendorLoginInput {
+    @IsEmail()
+    email: string;
+
+    @Length(6, 15)
+    password: string;
+}
+
+export interface CreateOfferInputs {
+    offerType: string;
+    vendors: [any];
+    title: string;
+    description: string;
+    minValue: number;
+    offerAmount: number;
+    startValidity: Date;
+    endValidity: Date;
+    promocode: string;
+    promoType: string;
+    bank: [any];
+    bins: [any];
+    pincode: string;
+    isActive: boolean;
+}
+
+export interface CreateTaxInputs {
+    name: string;
+    isApplicable: boolean;
+    rate: string;
+}
---
+++ src/routes/VendorRoute.ts
@@ -0,0 +1,82 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { AddCategory, AddEmployee, AddFood, AddOffer, AddRole, AddTable, EditOffer, EmployeeLogin, EmployeeVerify, GetCategories, GetAllEmployee, GetFoods, GetOffers, GetOrderDetails, GetOrders, GetPermissions, GetTables, GetVendorProfile, ProcessOrder, UpdateTable, UpdateVendorCoverImage, UpdateVendorProfile, UpdateVendorService, VendorLogin, EditRole, UpdateEmployeeDetails, EditTax, AddTax, GetTaxes } from '../controllers';
+import { Authenticate } from '../middleware';
+import multer from 'multer';
+import fs from 'fs';
+
+const router = express.Router();
+
+// multer configuration
+
+const directory = 'images';
+
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory);
+}
+
+const imageStorage = multer.diskStorage(
+    {
+        destination: function(req, file, cb){
+            cb(null, directory)
+        },
+        filename: function(req, file, cb){
+            cb(null, new Date().toISOString().replace(/[-:T.]/g, '')+'_'+file.originalname);
+        }
+    }
+)
+
+const images = multer({ storage: imageStorage}).array('images', 10);
+
+
+router.post('/login', VendorLogin);
+router.post('/employee/login', EmployeeLogin)
+router.patch('/employee/verify', EmployeeVerify)
+
+router.use(Authenticate)
+
+router.get('/profile', GetVendorProfile);
+router.patch('/profile', UpdateVendorProfile);
+router.patch('/coverimage', images, UpdateVendorCoverImage);
+router.patch('/service', UpdateVendorService);
+
+router.post('/category', images, AddCategory);
+router.get('/category', GetCategories);
+router.post('/food', images, AddFood);
+router.get('/food', GetFoods)
+
+router.post('/table', AddTable);
+router.patch('/table', UpdateTable);
+router.get('/table', GetTables)
+
+router.get('/orders', GetOrders);
+router.put('/order/:id/process', ProcessOrder);
+router.get('/order/:id', GetOrderDetails)
+
+router.post('/employee/create', AddEmployee)
+router.get('/employee', GetAllEmployee)
+router.patch('/employee', UpdateEmployeeDetails)
+
+router.post('/role', AddRole)
+router.patch('/role', EditRole)
+router.get('/permissions', GetPermissions)
+
+//Offers
+router.get('/offers', GetOffers);
+router.post('/offer', AddOffer);
+router.put('/offer/:id', EditOffer)
+ 
+router.get('/taxes', GetTaxes);
+router.post('/tax', AddTax);
+router.put('/tax/:taxId', EditTax)
+ 
+
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+ 
+res.json({ message: "Hello from Vandor"})
+
+})
+
+
+
+export { router as VandorRoute };
